295d604669bc3da39651a9220600ae2060253660
Julian Helmholz <39792510+JulHelm@users.noreply.github.com>
2020-03-02 12:13:37 +0100

    added navigation and crown svg

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import './pages/homepage/homepage.styles.scss';
 import ShopPage from './components/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
